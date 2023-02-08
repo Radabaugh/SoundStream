0f249ef48b9ac3688456915c600fb471de11bd58
Radabaugh <radabaughtyler24@gmail.com>
2023-02-08 13:07:50 -0800

    Create audio files by channel member

--- sound_stream.py
+++ sound_stream.py
@@ -19,3 +19,18 @@ async def join(ctx):
 @bot.command(name="leave", help="Leaves a voice channel")
 async def leave(ctx):
     await ctx.voice_client.disconnect()
+
+
+@bot.event
+async def on_member_update(self, member, before, after):
+    # Get the voice client for the voice channel the user is in
+    voice_client = discord.utils.get(self.voice_clients, guild=member.guild)
+
+    # Check if the user has joined a voice channel
+    if after.channel and voice_client and voice_client.is_connected():
+        user_stream = voice_client.source
+
+        # Output the user's audio stream to a separate file
+        with open(f"{member.id}_{member.name}.wav", "wb") as f:
+            async for chunk in user_stream:
+                f.write(chunk)
