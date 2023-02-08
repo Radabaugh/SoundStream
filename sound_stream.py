e8ec0c507687e0d8786a3bf139b3158a976c7de9
Radabaugh <radabaughtyler24@gmail.com>
2023-02-08 12:08:14 -0800

    Add Join command

--- sound_stream.py
+++ sound_stream.py
@@ -1,8 +1,16 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+
+bot = commands.Bot(command_prefix="!")
+
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has connected to Discord!')
+    print(f"{bot.user} has connected to Discord!")
+
+
+@bot.command(name="join", help="Joins a voice channel")
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
